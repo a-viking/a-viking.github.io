5ddfcf8f95f4655e287f3a5ec5ba4cb572f14b9d
Anton Televanov <anton.televanov@paradine.at>
2022-10-28 15:14:51 +0300

    service worker

--- js/main.js
+++ js/main.js
@@ -48,3 +48,17 @@ itemTypesContainer.addEventListener('click', ({ target }) => {
         renderImage();
     }
 });
+
+window.addEventListener("load", () => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            console.log(`SW registration failed`);
+        }
+    }
+}
